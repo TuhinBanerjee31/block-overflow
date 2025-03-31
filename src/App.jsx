06e7d9c226cf50ba3a9cbb6798b18311468f078b
somenjeet01 <somenjeet2004@gmail.com>
2025-04-01 01:40:12 +0530

    fixed the bugs!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { WalletProvider } from './context/WalletContext';
-import Header from './custom/Header';
-import QuestionsPage from './pages/QuestionsPage';
-import AnswerPage from './pages/AnswersPage';
-import ManagePage from './pages/ManagePage';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Footer from './custom/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { WalletProvider } from "./context/WalletContext";
+import Header from "./custom/Header";
+import QuestionsPage from "./pages/QuestionsPage";
+import AnswerPage from "./pages/AnswersPage";
+import ManagePage from "./pages/ManagePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./custom/Footer";
+import NotFound from "./pages/NotFound";
+import { PageTransition } from "./custom/page-transition";
 
 function App() {
   const toastOptions = {
@@ -16,7 +18,7 @@ function App() {
     closeOnClick: true,
     pauseOnHover: true,
     draggable: true,
-    theme: "light"
+    theme: "light",
   };
 
   return (
@@ -25,18 +27,47 @@ function App() {
         <div className="flex flex-col min-h-screen bg-gray-100">
           <ToastContainer {...toastOptions} />
           <Header />
-          <Routes>
-            <Route path="/" element={<QuestionsPage />} />
-            <Route path="/answer" element={<AnswerPage />} />
-            <Route path="/manage" element={<ManagePage />} />
-          </Routes>
+          <main className="flex-grow">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <PageTransition>
+                    <QuestionsPage />
+                  </PageTransition>
+                }
+              />
+              <Route
+                path="/answer"
+                element={
+                  <PageTransition>
+                    <AnswerPage />
+                  </PageTransition>
+                }
+              />
+              <Route
+                path="/manage"
+                element={
+                  <PageTransition>
+                    <ManagePage />
+                  </PageTransition>
+                }
+              />
+              <Route
+                path="*"
+                element={
+                  <PageTransition>
+                    <NotFound />
+                  </PageTransition>
+                }
+              />
+            </Routes>
+          </main>
           <Footer />
         </div>
       </BrowserRouter>
     </WalletProvider>
   );
-
-  
 }
 
 export default App;
